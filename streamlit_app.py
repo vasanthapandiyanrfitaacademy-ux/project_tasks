09e160f9ba0e35c3edbb27f5f156e092bf6f7ff5
vasanth <vasanthapandiyanrfitaacademy-ux>
2026-07-05 14:52:28 +0530

    some changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,13 +6,13 @@ make_sidebar()
 
 st.title("Welcome black to Pharmalytics powered by vasanth")
 
-st.write("Please log in to continue (username `guest`, password `guest123`).")
+st.write("Please log in to continue (username `vasanth`, password `vasu123`).")
 
 username = st.text_input("Username")
 password = st.text_input("Password", type="password")
 
 if st.button("Log in", type="primary"):
-    if username == "guest" and password == "guest123":
+    if username == "Vasanth" and password == "vasu123":
         st.session_state.logged_in = True
         st.success("Logged in successfully!")
         sleep(0.5)
