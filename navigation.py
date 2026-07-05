1b49812fe3b1867c76ccfede1ddcbe0d0d00a33d
vasanth <vasanthapandiyanrfitaacademy-ux>
2026-07-05 12:01:44 +0530

    name changes

--- navigation.py
+++ navigation.py
@@ -21,7 +21,7 @@ def make_sidebar():
         st.write("")
 
         if st.session_state.get("logged_in", False):
-            st.page_link("pages/page1.py", label="Home", icon="🏠")
+            st.page_link("pages/page1.py", label="Home main page", icon="")
             st.page_link("pages/page2.py", label="Dataset Analytics", icon="📈")
             st.page_link("pages/page3.py", label="Grouped Forecasting", icon="💰")            
             st.page_link("pages/page4.py", label="Single Forecasting", icon="💰")
